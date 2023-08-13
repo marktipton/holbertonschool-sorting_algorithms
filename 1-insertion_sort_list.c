50da22caa305d6625d8b2c7f40f2affb7ecd6fa9
marktipton <6675@holbertonstudent.com>
2023-08-12 23:11:24 -0700

    insertion v1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,13 +1,40 @@
 #include "sort.h"
 /**
- * selection_sort - sorts a doubly linked list in ascending order
+ * insertion_sort - sorts a doubly linked list in ascending order
  *
  * @array: array of integers
  * @size: size of array
  *
  */
-void selection_sort(int *array, size_t size)
+void insertion_sort_list(listint_t **list)
 {
+	listint_t *current = *list;
+	listint_t *temp;
+	listint_t *sort_node = NULL;
 
+	while (current != NULL)
+	{
+		temp = current->next;
 
+		while (sort_node != NULL && sort_node->n > current->n)
+		{
+			sort_node->prev->next = current;
+			current->next = sort_node;
+			sort_node = current;
+			current = temp;
+		}
+		if (sort_node == NULL)
+			sort_node = current;
+		else
+		{
+			current->prev->next = temp;
+			temp->prev = current->prev;
+			current->prev = sort_node;
+			sort_node->next = current;
+		}
+		print_list(*list);
+
+		current = temp;
+	}
+	*list = sort_node;
 }
