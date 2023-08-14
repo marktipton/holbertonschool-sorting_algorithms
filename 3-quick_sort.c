a22fe90da41be1325f09864926ed877fb963bf68
marktipton <6675@holbertonstudent.com>
2023-08-13 20:28:51 -0700

    quick sort lumoto

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
+#include "swap.c"
 /**
- * quick_sort - Entry point
+ * quick_sort - sorts array of integers into ascending order
  *
- * @array:
- * @size:
+ * @array: array of integers
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
+	int pivot;
+	size_t i = 0, j;
 
+	if (array == NULL || size <= 1)
+		return;
+	pivot = array[size - 1];
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			swap(&array[i], &array[j]);
+			i++;
+		}
+	}
+	swap(&array[i], &array[size - 1]);
+	
+	print_array(array, size);
+	quick_sort(array, i);
+	quick_sort(array + i + 1, size - i - 1);
 }
