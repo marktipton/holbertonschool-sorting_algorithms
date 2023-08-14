ea20d5910509e73f3fc7d8b41f4c6ba72d122af0
marktipton <6675@holbertonstudent.com>
2023-08-13 20:44:25 -0700

    changed back

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,8 +15,8 @@ void quick_sort(int *array, size_t size)
 
 	if (array == NULL || size <= 1)
 		return;
-	pivot = array[size];
-	for (j = 0; j < size; j++)
+	pivot = array[size - 1];
+	for (j = 0; j < size - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -25,7 +25,7 @@ void quick_sort(int *array, size_t size)
 			i++;
 		}
 	}
-	swap(&array[i], &array[size]);
+	swap(&array[i], &array[size - 1]);
 	quick_sort(array, i);
 	quick_sort(array + i + 1, size - i - 1);
 }
