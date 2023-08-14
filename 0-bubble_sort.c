b2d910eb0f21825b1a05e1c9cdd213362d03af4c
marktipton <6675@holbertonstudent.com>
2023-08-13 19:21:54 -0700

    compilation fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,7 +6,7 @@
  * @el_1: pointer to one element
  * @el_2: pointer to another element
  */
-void swap(int *el_1, int *ell_2)
+void swap(int *el_1, int *el_2)
 {
 	int temp = *el_1;
 	*el_1 = *el_2;
